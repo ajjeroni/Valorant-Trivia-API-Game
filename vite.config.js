7cccd806b4a5ef94ff1d925f5112560558b130f3
ajjeroni <adanjeronimo213@gmail.com>
2025-07-09 11:20:10 -0700

    fixing base on vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'Valorant-Trivia-API-Game',
+  base: '/Valorant-Trivia-API-Game/',
   plugins: [react()],
 })
